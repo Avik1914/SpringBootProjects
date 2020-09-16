260487989fa46029af9f8144a1e6e2150722604d
Avik Khamaru <tubla14@gmail.com>
2020-09-16 01:31:33 -0400

    Spring Work

---
+++ springboot-steps/src/main/java/com/avik/springboot/basics/springbootsteps/SpringbootStepsApplication.java
@@ -0,0 +1,13 @@
+package com.avik.springboot.basics.springbootsteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStepsApplication.class, args);
+	}
+
+}
---
+++ springboot-steps/src/test/java/com/avik/springboot/basics/springbootsteps/SpringbootStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avik.springboot.basics.springbootsteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
